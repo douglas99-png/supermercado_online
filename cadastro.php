ff79ef0bb548a757f87a7f610cc6428f7f985df4
Guilherme Araujo <guilhermecalixto813@gmail.com>
2025-05-24 22:44:53 -0300

    cadastro.php

--- cadastro.php
+++ cadastro.php
@@ -7,6 +7,25 @@
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
+
+<header>
+    <h1>Supermercado Online</h1>
+    <nav>
+        <a href="index.php">Início</a>
+        <a href="produtos.php">Produtos</a>
+        <a href="ofertas.php">Ofertas</a>
+        <a href="carrinho.php">🛒 Carrinho</a>
+        <a href="contato.php">Contato</a>
+        <?php if(isset($_SESSION['id'])): ?>
+            <a href="painel.php">Painel</a>
+            <a href="logout.php">Sair</a>
+        <?php else: ?>
+            <a href="login.php">Login</a>
+            <a href="cadastro.php">Cadastro</a>
+        <?php endif; ?>
+    </nav>
+</header>
+
 <div class="container">
     <h1>Cadastro</h1>
     <form action="processa_cadastro.php" method="POST">
@@ -17,5 +36,7 @@
     </form>
     <a href="login.php">Já tem conta? Login</a>
 </div>
+
+<footer>© 2025 Supermercado Online</footer>
 </body>
 </html>
